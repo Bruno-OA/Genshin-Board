def5278c865a83156fa160950865725440c328cb
Bruno-OA <59889123+Bruno-OA@users.noreply.github.com>
2026-02-23 15:20:53 -0300

    Add files via upload

---
+++ genshin-board/genshin-board/src/main/java/com/genshin/board/genshin_board/GenshinBoardApplication.java
@@ -0,0 +1,13 @@
+package com.genshin.board.genshin_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinBoardApplication.class, args);
+	}
+
+}
---
+++ genshin-board/genshin-board/src/test/java/com/genshin/board/genshin_board/GenshinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genshin.board.genshin_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenshinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
